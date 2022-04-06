712f3d7961d3fe3d23bea2b7d13cf0b6b4920d3a
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-04-06 20:16:20 +0100

    added session to server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const session=require("express-session");
 const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
-
+const dbConnection = require('./config/connection');
+const SequelizeStore=require("connect-session-sequelize")(session.Store)
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,6 +15,20 @@ const hbs = exphbs.create({ helpers });
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+const sess = {
+  name: "session",
+  secret: "it is my blog keep the secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  store: new SequelizeStore({
+    db: dbConnection,
+  }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+dbConnection.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
